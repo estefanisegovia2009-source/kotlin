792555948ccb003eb6d375fbc8f72e5132bf12dc
Estefani Segovia <estefani.segovia2009@gmail.com>
2026-04-25 23:37:02 -0300

    Actualizado

--- ds-practico-kotlin-fundamentos-main/fundamentos-basicos/src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
+++ ds-practico-kotlin-fundamentos-main/fundamentos-basicos/src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
@@ -1,21 +1,66 @@
 package edu.etec.ds.fundamentos
 
 fun evaluarPositivo(numero: Int): String {
-    TODO("Retornar \"Positivo\" si numero > 0, sino \"No positivo\"")
+    if(numero > 0){
+        return "Positivo"
+    }else{
+        return "No positivo"
+    }
 }
 
 fun clasificarNumero(numero: Int): String {
-    TODO("Clasificar: Mayor a 10, Menor a 10, o Igual a 10")
+    if(numero > 10){
+        return "Mayor a 10"
+    }else if(numero < 10){
+        return "Menor a 10"
+    }else{
+        return "Igual a 10"
+    }
 }
 
-fun evaluarEdad(edad: Int): String {
-    TODO("Menor de edad: < 18, Adulto: 18-65, Adulto mayor: > 65")
+fun evaluarEdad(edad: Int): String{
+    if(edad < 18){
+        return "Menor de edad"
+    }else if(edad <= 65){
+        return "Adulto"
+    }else{
+        return "Adulto mayor"
+    }
 }
 
-fun obtenerDescuento(monto: Double): Double {
-    TODO("Retornar descuento: 10% si monto > 100, 20% si > 150, 30% si > 200, sino 0%")
+
+fun obtenerDescuento(monto: Double): Double  {
+    if(monto >= 200){
+        return 30.0
+    }else if(monto >=150){
+        return 20.0
+    }else if(monto >100){
+        return 10.0
+    }else{
+        return 0.0
+    }
 }
 
-fun calcular(operador: Char, a: Int, b: Int): Double {
-    TODO("Realizar operacion: + - * /. Retornar 0.0 si division por cero u operador invalido")
+fun calcular(operador: Char, a: Int, b: Int): Double e {
+
+    if(operador == '+'){
+        return (a+b).toDouble()
+    }
+
+    if(operador == '-'){
+        return (a-b).toDouble()
+    }
+
+    if(operador == '*'){
+        return (a*b).toDouble()
+    }
+
+    if(operador == '/'){
+        if(b==0){
+            return 0.0
+        }
+        return (a.toDouble()/b)
+    }
+
+    return 0.0
 }
